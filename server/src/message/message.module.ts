6a870764c685751582875ecf39d29d54681bb276
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-03-29 23:49:41 -0300

    fix: injetando a dependencia da aws no module de messages

--- server/src/message/message.module.ts
+++ server/src/message/message.module.ts
@@ -3,9 +3,10 @@ import { MessageService } from "./message.service";
 import { MessageController } from "./message.controller";
 import { PrismaService } from "../services/prisma";
 import { UserService } from "../user/user.service";
+import AWS from "../services/aws";
 
 @Module({
 	controllers: [MessageController],
-	providers: [MessageService, PrismaService, UserService],
+	providers: [MessageService, PrismaService, UserService, AWS],
 })
 export class MessageModule {}
